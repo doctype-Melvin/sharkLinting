3021681a9e644ca524839a39993e3acd2457c4be
Melvin Speckamp <doctype.melvin@gmail.com>
2023-03-27 16:34:47 +0200

    fixed toggle button issues

---
+++ index.js
@@ -0,0 +1,14 @@
+function toggleButton() {
+  const cards = document.querySelectorAll('[data-js=card]');
+  cards.forEach(card => {
+    const button = card.querySelector('[data-js="button"]');
+    const link = card.querySelector('[data-js="link"]');
+    const arrow = card.querySelector('[data-js="arrow"]');
+    button.addEventListener('click', () => {
+      link.classList.toggle('hidden');
+      arrow.classList.toggle('rotate');
+    });
+  });
+}
+
+toggleButton();
